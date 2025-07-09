78bb11f0eabc2c0bde580c3344190079b6a80d58
Andrew Grant <14335009+AndrewGrant31@users.noreply.github.com>
2025-07-09 15:40:42 -0400

    deleted out/build (not needed and shouldn't be here at all)

--- out/build/clang-x64-debug/qtcsettings.cmake
+++
@@ -1,4 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_CXX_COMPILER" "clang++.exe C:/Program Files/LLVM/bin/clang++.exe" CACHE "UNINITIALIZED" "" FORCE)
-set("CMAKE_C_COMPILER" "clang.exe C:/Program Files/LLVM/bin/clang.exe" CACHE "UNINITIALIZED" "" FORCE)
